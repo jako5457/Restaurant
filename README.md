aeeead570b41a43562e64d8a5017e09acaed2dc8
ECR\Egon Rasmussen <ecr@live.dk>
2020-05-06 11:23:56 +0200

    Core 3.1

--- README.md
+++ README.md
@@ -3,15 +3,16 @@ Der oprettes en mulighed for at redigere data for restauranterne:
 
 -- Her indsættes billede af Edit-viewet
 
-### ServiceLayer
+## ServiceLayer
 RestaurantService udvides med en Edit metode:
 
 
-### WebApp
+## WebApp
 Her tilføjes...
 
-Her ses en anden måde at binde en enum til en Select, hvor det alene foregår i View'et. Her benyttes metoden: ```Html.GetEnumSelectList<T>()```:
-```c#
+#### SELECT
+Her er benyttet den simple måde at binde en enum til en Select, hvor det alene foregår i View'et. Her benyttes metoden: ```Html.GetEnumSelectList<T>()```:
+```xml
 <div class="form-group">
     <label asp-for="Restaurant.Cuisine" />
     <select asp-for="Restaurant.Cuisine"
@@ -20,6 +21,30 @@ Her ses en anden måde at binde en enum til en Select, hvor det alene foregår i V
     <span class="text-danger" asp-validation-for="Restaurant.Cuisine"></span>
 </div>
 ```
+#### Alternativ binding til SELECT
+En anden måde er at injecte `IHtmlHelper` servicen ind i ViewModel-klassens constructor (kræver import af namespacet: `Microsoft.AspNetCore.Mvc.Rendering`):
+```csharp
+public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+private IHtmlHelper _htmlHelper;
+
+public EditModel(IRestaurantService restaurantService, IHtmlHelper htmlHelper)
+{
+    _restaurantService = restaurantService;
+    _htmlHelper = htmlHelper;
+}
+```
+I OnGet hentes data ind på følgende måde:
+```csharp
+Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
+```
+
+Og i View'et bindes SELECT kontrollen til Cuisines property:
+```xml
+asp-items="Model.Cuisines"
+```
+
+
 ---
 ## Post-Redirect-Get Pattern
 Beskrivelse
--- WebApp/Pages/Restaurants/Edit.cshtml.cs
+++ WebApp/Pages/Restaurants/Edit.cshtml.cs
@@ -1,9 +1,7 @@
 ﻿using DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using ServiceLayer;
-using System.Collections.Generic;
 
 namespace WebApp.Pages.Restaurants
 {
@@ -11,21 +9,16 @@ namespace WebApp.Pages.Restaurants
     {
         [BindProperty]
         public Restaurant Restaurant { get; set; }
-        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
         private readonly IRestaurantService _restaurantService;
-        private IHtmlHelper _htmlHelper;
 
-        public EditModel(IRestaurantService restaurantService, IHtmlHelper htmlHelper)
+        public EditModel(IRestaurantService restaurantService)
         {
             _restaurantService = restaurantService;
-            _htmlHelper = htmlHelper;
         }
 
         public IActionResult OnGet(int? restaurantId)
         {
-            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
-
             if (restaurantId.HasValue)
             {
                 Restaurant = _restaurantService.GetRestaurantById(restaurantId.Value);
@@ -47,7 +40,6 @@ namespace WebApp.Pages.Restaurants
         {
             if (!ModelState.IsValid)
             {
-                Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
                 return Page();
                
             }
@@ -60,6 +52,7 @@ namespace WebApp.Pages.Restaurants
                 _restaurantService.Add(Restaurant);
             }
             _restaurantService.Commit();
+
             TempData["Message"] = "Restaurant saved!";
             return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
         }
